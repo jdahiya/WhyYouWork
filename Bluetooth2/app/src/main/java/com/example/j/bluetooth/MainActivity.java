bc070a63da6a310d874128bf4b47615890fe8735
Derek Chan <derekchan1994@gmail.com>
2016-03-13 04:49:30 -0700

    Jays buttons, i hate michelle

--- Bluetooth2/app/src/main/java/com/example/j/bluetooth/MainActivity.java
+++ Bluetooth2/app/src/main/java/com/example/j/bluetooth/MainActivity.java
@@ -24,8 +24,27 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.telephony.SmsManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     Button btnOn, btnOff;
     TextView txtArduino, txtString, txtStringLength, sensorView0, sensorView1, sensorView2, sensorView3;
@@ -44,6 +63,12 @@ public class MainActivity extends Activity {
     // String for MAC address
     private static String address;
 
+    //For UI
+    public static boolean HAS_BUTT_SENSOR = false;
+    public static boolean BUTT_PROBLEM = false;
+
+    private boolean showingFront = true;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,7 +78,7 @@ public class MainActivity extends Activity {
         //Link the buttons and textViews to respective views
         btnOn = (Button) findViewById(R.id.btnOn);
         btnOff = (Button) findViewById(R.id.btnOff);
-        txtString = (TextView) findViewById(R.id.txtArduino);
+       // txtString = (TextView) findViewById(R.id.txtArduino);
 
 
         bluetoothIn = new Handler() {
@@ -82,6 +107,48 @@ public class MainActivity extends Activity {
                 Toast.makeText(getBaseContext(), "Turn on LED", Toast.LENGTH_SHORT).show();
             }
         });
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddWearableDialogFragment wearableFragment = new AddWearableDialogFragment();
+                wearableFragment.setOnAddClickListener(new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        HAS_BUTT_SENSOR = true;
+                        drawButt();
+                    }
+                });
+                wearableFragment.show(getSupportFragmentManager(), "wearable");
+            }
+        });
+
+        fab = (FloatingActionButton) findViewById(R.id.flip_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showingFront = !showingFront;
+                drawButt();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle); // WHY DID U USE SDK 6.0 U TOLD US 5.0
+        //drawer.addDrawerListener(toggle); // GDI MICHELLE
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 1);
     }
 
     private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
@@ -204,5 +271,99 @@ public class MainActivity extends Activity {
 
             }
         }
+    }
+
+    private void drawButt() {
+        ImageView imageView = (ImageView) findViewById(R.id.image_body);
+
+        if (showingFront) {
+            imageView.setImageResource(R.drawable.body_front);
+        } else {
+            if (HAS_BUTT_SENSOR) {
+                if (BUTT_PROBLEM) {
+                    imageView.setImageResource(R.drawable.body_back_butt_red);
+                } else {
+                    imageView.setImageResource(R.drawable.body_back_butt_green);
+                }
+            } else {
+                imageView.setImageResource(R.drawable.body_back);
+            }
+        }
+    }
+
+    public void playSound() {
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        r.play();
+    }
+
+    public void showButtNotification() {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setContentTitle("No Pressure")
+                        .setContentText("You may want to adjust the pressure on your buttocks.");
+    }
+
+    public void sendButtSMS() {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("6043656015", null,
+                "Your patient may need to adjust the pressure on his or her buttocks.", null, null);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
